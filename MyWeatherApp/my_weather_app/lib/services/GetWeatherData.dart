b6662a6051c9565d4c64f7cb5486b61867557308
Shubham Vimal <51742764+ShubhamVimal@users.noreply.github.com>
2021-07-08 10:29:20 +0530

    Minor fix

--- MyWeatherApp/my_weather_app/lib/services/GetWeatherData.dart
+++ MyWeatherApp/my_weather_app/lib/services/GetWeatherData.dart
@@ -26,7 +26,7 @@ class GetWeatherData {
   static Future<WeatherData?> searchWeatherData({required String city}) async {
     try {
       var response = await client.get(
-          'http://api.weatherapi.com/v1/forecast.json?key=9028ee0c1281479d812144856212406&q=$city&days=3&aqi=yes&alerts=no');
+          'http://api.weatherapi.com/v1/forecast.json?key={YOUR_API_KEY}&q=$city&days=3&aqi=yes&alerts=no');
       if (response.statusCode == 200) {
         var jsonData = response.data;
 
